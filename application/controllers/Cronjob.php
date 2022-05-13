196c7baa889a7757268dc398acbdc18958dd88b6
Zheng Zhi Ling <zyema2212@gmail.com>
2022-05-14 02:18:20 +0800

    line_message_cronjob

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -21,6 +21,20 @@ class Cronjob extends EA_Controller {
         $day_start = $remind_time;
         $day_end = $remind_time +1;
         $appointments = $this->appointments_model->get_scheduled_appointments($day_start, $day_end);
+
+        if(count($appointments) > 0){
+            $settings = [
+                'company_name' => $this->settings_model->get_setting('company_name'),
+                'company_address' => $this->settings_model->get_setting('company_address'),
+            ];
+            foreach ($appointment as $appointments) {
+                $provider = $this->providers_model->get_row($appointment['id_users_provider']);
+                $service = $this->services_model->get_row($appointment['id_services']);
+                $customer = $this->customers_model->get_row($appointment['id_users_customer']);
+                line_message_cronjob($setting, $provider, $service, $customer);
+            }
+        }
+
         print_r($appointments);
         // echo json_encode($datetime);
     }
